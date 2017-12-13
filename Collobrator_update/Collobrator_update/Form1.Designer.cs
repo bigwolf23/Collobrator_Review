11e48bcc6d99c1e221becc2ac078e6916b4fea56
bigwolf23 <47907541@QQ.com>
2017-12-13 23:04:50 +0800

    First Version

---
+++ Collobrator_update/Collobrator_update/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Collobrator_update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.radio_NewReview = new System.Windows.Forms.RadioButton();
+            this.radio_OldReview = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClearCaseFilePath = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ClearCase_MapPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClearCase_ReviewID = new System.Windows.Forms.TextBox();
+            this.ClearCase_ReviewID_label = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(591, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update_Review";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radio_NewReview
+            // 
+            this.radio_NewReview.AutoSize = true;
+            this.radio_NewReview.Checked = true;
+            this.radio_NewReview.Location = new System.Drawing.Point(15, 15);
+            this.radio_NewReview.Name = "radio_NewReview";
+            this.radio_NewReview.Size = new System.Drawing.Size(77, 16);
+            this.radio_NewReview.TabIndex = 1;
+            this.radio_NewReview.TabStop = true;
+            this.radio_NewReview.Text = "NewReview";
+            this.radio_NewReview.UseVisualStyleBackColor = true;
+            this.radio_NewReview.CheckedChanged += new System.EventHandler(this.radio_NewReview_CheckedChanged);
+            // 
+            // radio_OldReview
+            // 
+            this.radio_OldReview.AutoSize = true;
+            this.radio_OldReview.Location = new System.Drawing.Point(154, 15);
+            this.radio_OldReview.Name = "radio_OldReview";
+            this.radio_OldReview.Size = new System.Drawing.Size(77, 16);
+            this.radio_OldReview.TabIndex = 1;
+            this.radio_OldReview.Text = "OldReview";
+            this.radio_OldReview.UseVisualStyleBackColor = true;
+            this.radio_OldReview.CheckedChanged += new System.EventHandler(this.radio_OldReview_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radio_OldReview);
+            this.groupBox1.Controls.Add(this.radio_NewReview);
+            this.groupBox1.Location = new System.Drawing.Point(10, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 37);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Review_mode";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ClearCase_CommitFile";
+            // 
+            // ClearCaseFilePath
+            // 
+            this.ClearCaseFilePath.Location = new System.Drawing.Point(143, 69);
+            this.ClearCaseFilePath.Name = "ClearCaseFilePath";
+            this.ClearCaseFilePath.Size = new System.Drawing.Size(554, 21);
+            this.ClearCaseFilePath.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 262);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ClearCase_MapPath
+            // 
+            this.ClearCase_MapPath.Location = new System.Drawing.Point(143, 42);
+            this.ClearCase_MapPath.Name = "ClearCase_MapPath";
+            this.ClearCase_MapPath.Size = new System.Drawing.Size(232, 21);
+            this.ClearCase_MapPath.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ClearCase_MapPath";
+            // 
+            // ClearCase_ReviewID
+            // 
+            this.ClearCase_ReviewID.Location = new System.Drawing.Point(512, 41);
+            this.ClearCase_ReviewID.MaxLength = 6;
+            this.ClearCase_ReviewID.Name = "ClearCase_ReviewID";
+            this.ClearCase_ReviewID.Size = new System.Drawing.Size(185, 21);
+            this.ClearCase_ReviewID.TabIndex = 10;
+            this.ClearCase_ReviewID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ClearCase_ReviewID_KeyPress);
+            // 
+            // ClearCase_ReviewID_label
+            // 
+            this.ClearCase_ReviewID_label.AutoSize = true;
+            this.ClearCase_ReviewID_label.Location = new System.Drawing.Point(382, 43);
+            this.ClearCase_ReviewID_label.Name = "ClearCase_ReviewID_label";
+            this.ClearCase_ReviewID_label.Size = new System.Drawing.Size(113, 12);
+            this.ClearCase_ReviewID_label.TabIndex = 9;
+            this.ClearCase_ReviewID_label.Text = "ClearCase_ReviewId";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 391);
+            this.Controls.Add(this.ClearCase_ReviewID);
+            this.Controls.Add(this.ClearCase_ReviewID_label);
+            this.Controls.Add(this.ClearCase_MapPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ClearCaseFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Review_Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RadioButton radio_NewReview;
+        private System.Windows.Forms.RadioButton radio_OldReview;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ClearCaseFilePath;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox ClearCase_MapPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ClearCase_ReviewID;
+        private System.Windows.Forms.Label ClearCase_ReviewID_label;
+    }
+}
+
---
+++ Collobrator_update/Collobrator_update/Form1.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Collobrator_update
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string strCollobarate_new_cmd = "ccollab addversions new";
+        //public static string strCollobarate_cmd = "ipconfig";
+        public static string strNew_keyword = "New review created: Review #";
+        public static string strCheckIn_keyword = "CHECKEDIN";
+        public static string strCheckOut_keyword = "CheckedOut:";
+        /// <summary>
+        /// Cmd 的摘要说明。
+        /// </summary>b
+        public class Cmd
+        {
+            private Process proc = null;
+            /// <summary>
+            /// 构造方法
+            /// </summary>
+            public Cmd()
+            {
+                proc = new Process();
+            }
+            /// <summary>
+            /// 执行CMD语句
+            /// </summary>
+            /// <param name="cmd">要执行的CMD命令</param>
+            public string RunCmd(string cmd)
+            {
+                proc.StartInfo.CreateNoWindow = false;
+                proc.StartInfo.FileName = "cmd.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.Start();
+                proc.StandardInput.WriteLine(cmd);
+                proc.StandardInput.WriteLine("exit");
+                string outStr = proc.StandardOutput.ReadToEnd();
+                proc.Close();
+                return outStr;
+            }
+            /// <summary>
+            /// 打开软件并执行命令
+            /// </summary>
+            /// <param name="programName">软件路径加名称（.exe文件）</param>
+            /// <param name="cmd">要执行的命令</param>
+            public void RunProgram(string programName, string cmd)
+            {
+                Process proc = new Process();
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.FileName = programName;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.Start();
+                if (cmd.Length != 0)
+                {
+                    proc.StandardInput.WriteLine(cmd);
+                }
+                proc.Close();
+            }
+            /// <summary>
+            /// 打开软件
+            /// </summary>
+            /// <param name="programName">软件路径加名称（.exe文件）</param>
+            public void RunProgram(string programName)
+            {
+                this.RunProgram(programName, "");
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            controlShowOrHide(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ClearCase_MapPath.Text.Length == 0)
+            {
+                MessageBox.Show(@"Please input ClearCase_MapPath!!!");
+                return;
+            }
+
+            if(radio_NewReview.Checked == true)
+            {
+                updateReviewFile(@"new");
+            }
+            else
+            {
+                if (ClearCase_ReviewID.Text.Length == 6 )
+                {
+                    updateReviewFile(ClearCase_ReviewID.Text);
+                }
+                else
+                {
+                    MessageBox.Show(@"Because this is Add to old review,please input old review ID");
+                }
+            }
+        }
+
+        
+
+
+        private void getClearCaseFilePath(ref List<string> strFileListOut)
+        {
+            string ClearcaseFilePath = ClearCaseFilePath.Text;
+            //strFilePath = @"G:\Project\My_Source\Collobrater_update\Review_content\update.2017-12-07T103338+0800.updt";
+            if (File.Exists(ClearcaseFilePath))
+            {
+                getClearCaseFileList(ClearcaseFilePath, ref strFileListOut);
+            }
+        }
+
+        private void getClearCaseFileList(string ClearcaseFilePath,ref List<string> strFileList)
+        {
+            FileStream fs = new FileStream(ClearcaseFilePath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            while (sr.Peek() >= 0)
+            {
+                string strLine = sr.ReadLine();
+                if (strLine.Contains(strCheckOut_keyword) && strLine.Contains(strCheckIn_keyword))
+                {
+                    strFileList.Add(strLine);
+                }
+            }
+            sr.Close();
+            fs.Close();
+        }
+
+        public class DisplayData
+        {
+            public string FilePath { get; set; }
+            public string strFileConvertSuccess { get; set; }
+            public string strDecription { get; set; }
+        }
+
+
+        private void updateReviewFile(string strReviewId)
+        {
+            List<string> strFileList = new List<string>();
+            List<string> m_pFileFullPaths = new List<string>();
+            bool bReturn = false;
+            getClearCaseFilePath(ref strFileList);
+            if (strReviewId.Length == 6)
+            {
+                bReturn = getOldReviewFilePath(strReviewId, strFileList, ref m_pFileFullPaths);
+            }
+            else
+            {
+                bReturn = getNewReviewFilePath(strReviewId, strFileList, ref m_pFileFullPaths);
+            }
+            List<DisplayData> m_pDisplayData = new List<DisplayData>();
+
+            foreach (string strTemp in m_pFileFullPaths)
+            {
+                DisplayData temp = new DisplayData();
+                temp.strFileConvertSuccess = "Fail";
+                temp.strFileConvertSuccess = runCmd(strTemp);
+                m_pDisplayData.Add(temp);
+            }
+            displayDataFalseOrSuccess(m_pDisplayData);
+        }
+
+        private bool getNewReviewFilePath(string strReviewId, List<string> strFileList, ref List<string> FileFullPaths)
+        {
+            string strFileFormatAll;
+            string strNewReviewId = strReviewId;
+            bool bFirstCmd = true;
+            foreach (string strTemp in strFileList)
+            {
+                string strNewPath;
+                string strOldPath;
+                
+                getFilePathByClearCaseResult(strTemp, out strNewPath, out strOldPath);
+
+                strFileFormatAll = @"ccollab addversions " +
+                    strNewReviewId + @"  " + "\"" +
+                    strNewPath + "\"" + "\"" +
+                    strOldPath + "\"";
+                if (bFirstCmd)
+                {
+                    Cmd c = new Cmd();
+                    string strCmdReturn = c.RunCmd(strFileFormatAll);
+                    if (strCmdReturn.Contains(@"successful"))
+                    {
+                        MessageBox.Show(strCmdReturn);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+
+                    if (strCmdReturn.Contains(strNew_keyword))
+                    {
+                        strNewReviewId = strCmdReturn.Substring(strNew_keyword.Length, 6);
+                    }
+                }
+                
+                bFirstCmd = false;
+
+                //@"ccollab addversions ";
+                FileFullPaths.Add(strFileFormatAll);
+                
+            }
+            return true;
+        }
+
+        private bool getOldReviewFilePath(string strReviewId, List<string> strFileList, ref List<string> FileFullPaths)
+        {
+            foreach (string strTemp in strFileList)
+            {
+                string strNewPath;
+                string strOldPath;
+                string strFileFormatAll;
+                getFilePathByClearCaseResult(strTemp, out strNewPath,out strOldPath);
+
+                strFileFormatAll = @"ccollab addversions " +
+                    strReviewId + @"  " + "\"" +
+                    strNewPath + "\"" + "\"" +
+                    strOldPath + "\"";
+
+                //@"ccollab addversions ";
+                FileFullPaths.Add(strFileFormatAll);
+            }
+            return true;
+        }
+
+        private void getFilePathByClearCaseResult(string strResult,out string strNewPath,out string strOldPath)
+        {
+            int nCheckinWordPos = strResult.IndexOf(strCheckIn_keyword, 0);
+            string strFilePathOld = strResult.Substring(25, nCheckinWordPos - 25);
+            int nFileVerInClearCasePos = strFilePathOld.LastIndexOf(@"\");
+            string strFileVerInClearCase = strFilePathOld.Substring(nFileVerInClearCasePos + 1);
+            int nFileVerOld = int.Parse(strFileVerInClearCase);
+            int nFileVerNew = nFileVerOld + 1;
+
+            if (strFilePathOld.Contains(@" \main\int_"))
+            {
+                strFilePathOld = strFilePathOld.Replace(@" \main\int_", @"@@\main\int_");
+            }
+            else
+            {
+                strFilePathOld = strFilePathOld.Replace(@" \main\1", @"@@\main\int_10.00.00_dae\0");
+            }
+
+            strFilePathOld = ClearCase_MapPath.Text + strFilePathOld;
+            //strFilePathOld = @"Z:\" + strFilePathOld;
+
+            string strFilePathNew = strFilePathOld;
+            nFileVerInClearCasePos = strFilePathNew.LastIndexOf(@"\");
+            strFilePathNew = strFilePathNew.Remove(nFileVerInClearCasePos + 1);
+            strFilePathNew = strFilePathNew + Convert.ToString(nFileVerNew);
+
+            strNewPath = strFilePathNew;
+            strOldPath = strFilePathOld;
+        }
+
+        private void addNewReviewFile(string strReviewId)
+        {
+            List<string> strFileList = new List<string>();
+            List<string> m_pFileFullPaths = new List<string>();
+            getClearCaseFilePath(ref strFileList);
+            getNewReviewFilePath(strReviewId, strFileList, ref m_pFileFullPaths);
+            List<DisplayData> m_pDisplayData = new List<DisplayData>();
+
+            foreach (string strTemp in m_pFileFullPaths)
+            {
+                DisplayData temp = new DisplayData();
+                temp.strFileConvertSuccess = "Fail";
+                temp.strFileConvertSuccess = runCmd(strTemp);
+                m_pDisplayData.Add(temp);
+            }
+            displayDataFalseOrSuccess(m_pDisplayData);
+        }
+
+        private void getColobaratorFilePath(string strReviewId, List<string> strFileList)
+        {
+            List<DisplayData> m_pFileFullPaths = new List<DisplayData>();
+            bool bFistInsert = true;
+            string strReviewIdTemp = strReviewId;
+            foreach (string strTemp in strFileList)
+            {
+                int nCheckinWordPos = strTemp.IndexOf(strCheckIn_keyword,0);
+                string strFilePathOld = strTemp.Substring(25,nCheckinWordPos-25);
+                int nFileVerInClearCasePos = strFilePathOld.LastIndexOf(@"\");
+                string strFileVerInClearCase = strFilePathOld.Substring(nFileVerInClearCasePos + 1);
+                int nFileVerOld = int.Parse(strFileVerInClearCase);
+                int nFileVerNew = nFileVerOld + 1;
+
+                strFilePathOld = strFilePathOld.Replace(@" \main\int_", @"@@\main\int_");
+                strFilePathOld = ClearCase_MapPath.Text + strFilePathOld;
+                //strFilePathOld = @"Z:\" + strFilePathOld;
+
+                string strFilePathNew = strFilePathOld;
+                nFileVerInClearCasePos = strFilePathNew.LastIndexOf(@"\");
+                strFilePathNew = strFilePathNew.Remove(nFileVerInClearCasePos+1);
+                strFilePathNew = strFilePathNew + Convert.ToString(nFileVerNew);
+
+                string strFileFormatAll;
+                bFistInsert = false;
+
+                DisplayData temp = new DisplayData();
+                
+                temp.strFileConvertSuccess = "Fail";
+
+                if (bFistInsert)
+                {
+                    strFileFormatAll = strCollobarate_new_cmd +
+                    @"  " + "\"" +
+                    strFilePathNew + "\"" + "\"" +
+                    strFilePathOld + "\"";
+
+                    temp.FilePath = strFileFormatAll;
+                    temp.strFileConvertSuccess = runNewCmd(strFileFormatAll, ref strReviewIdTemp);
+                }
+                else
+                {
+                    strFileFormatAll = @"ccollab addversions " +
+                    strReviewId + @"  " + "\"" +
+                    strFilePathNew + "\"" + "\"" +
+                    strFilePathOld + "\"";
+
+                    temp.FilePath = strFileFormatAll;
+                    temp.strFileConvertSuccess = runCmd(strFileFormatAll);
+                }
+
+                //@"ccollab addversions ";
+                m_pFileFullPaths.Add(temp);
+            }
+            displayDataFalseOrSuccess(m_pFileFullPaths);
+        }
+
+        private string getVersionNo()
+        {
+            string sVersionNo = "";
+            Cmd c = new Cmd();
+            string strCmdReturn = c.RunCmd(strCollobarate_new_cmd);
+            //string strCmdReturn = c.RunCmd(@"ping 192.168.1.1");
+            MessageBox.Show(strCmdReturn);
+            if (strCmdReturn.Contains(strNew_keyword))
+            {
+                sVersionNo = strCmdReturn.Substring(strNew_keyword.Length, 6);
+            }
+            return sVersionNo;
+        }
+
+        private string runCmd(string strCmd)
+        {
+            Cmd c = new Cmd();
+            string strCmdReturn = c.RunCmd(strCmd);
+            if (strCmdReturn.Contains(@"successful"))
+            {
+                MessageBox.Show(strCmdReturn);
+            }
+
+            return @"Success";
+        }
+
+        private string runNewCmd(string strCmd, ref string strVerionId)
+        {
+            Cmd c = new Cmd();
+            string strCmdReturn = c.RunCmd(strCmd);
+            if (strCmdReturn.Contains(@"successful"))
+            {
+                MessageBox.Show(strCmdReturn);
+            }
+            else
+            {
+                return @"Fail";
+            }
+
+            if (strCmdReturn.Contains(strNew_keyword))
+            {
+                strVerionId = strCmdReturn.Substring(strNew_keyword.Length, 6);
+            }
+
+            return @"Success";
+        }
+
+        private void displayDataFalseOrSuccess(List<DisplayData> strCollobarateFileFormat)
+        {
+            BindingList<DisplayData> BFileInfo = new BindingList<DisplayData>(strCollobarateFileFormat);
+            dataGridView1.DataSource = BFileInfo;
+        }
+
+        private void controlShowOrHide(bool bShow)
+        {
+            if (bShow)
+            {
+                ClearCase_ReviewID_label.Show();
+                ClearCase_ReviewID.Show();
+            }
+            else
+            {
+                ClearCase_ReviewID_label.Hide();
+                ClearCase_ReviewID.Hide();
+            }
+        }
+        private void radio_NewReview_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radio_NewReview.Checked == true)
+            {
+                controlShowOrHide(false);
+            }
+        }
+
+        private void radio_OldReview_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radio_OldReview.Checked == true)
+            {
+                controlShowOrHide(true);
+            }
+        }
+
+        private void ClearCase_ReviewID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 0x20) e.KeyChar = (char)0;  //禁止空格键
+            if ((e.KeyChar == 0x2D) && (((TextBox)sender).Text.Length == 0)) return;   //处理负数
+            if (e.KeyChar > 0x20)
+            {
+                try
+                {
+                    double.Parse(((TextBox)sender).Text + e.KeyChar.ToString());
+                }
+                catch
+                {
+                    e.KeyChar = (char)0;   //处理非法字符
+                }
+            }
+        }
+
+    }
+}
